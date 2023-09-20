9e834e1797faa5e782a71fb1b6ae9531ada1e276
ralph.moran <ralphmoran2003@gmail.com>
2023-09-19 21:28:50 -0700

    Edited index view

--- views/common/footer.php
+++ views/common/footer.php
@@ -1,9 +1,4 @@
-        <div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-            <div class="d-flex flex-center fw-bold fs-6">
-                <p>
-                    <?= date("Y") ?> © Aero Admin <?= env('APP_VERSION') ?> power by Aero Technologies Inc. 
-                </p>
-            </div>
-        </div>
+
+        <script src="" async defer></script>
     </body>
 </html>
--- views/common/header.php
+++ views/common/header.php
@@ -1,32 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
-<!--begin::Head-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php printf('Aeros %s | %s', env('FRAMEWORK_VERSION'), env('WEB_TITLE'))?></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
 
-<head>
-    <meta charset="utf-8" />
-    <title><?= empty($title) ? env('WEB_TITLE') : $title; ?></title>
-    <meta name="description" content="<?= env('WEB_DESCRIPTION'); ?>" />
-    <meta name="keywords" content="<?= env('WEB_KEYWORDS'); ?>" />
-    <meta name="viewport" content="<?= env('WEB_VIEWPORT'); ?>" />
-    <link rel="shortcut icon" href="<?= env('WEB_ICON'); ?>" />
-    <!--begin::Fonts-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-    <?= (basename($_SERVER['PHP_SELF']) == "deal") ? '<link href="assets/plugins/custom/vis-timeline/vis-timeline.bundle.css" rel="stylesheet" type="text/css" />' : '' ; ?>    
-    <? include_once(env('APP_ROOT_DIR') . "/static/headerscripts.php"); ?>
-    <script src="https://unpkg.com/autonumeric"></script>
-    <!--end::Global Stylesheets Bundle-->
-</head>
-<!--end::Head-->
-<!--begin::Body-->
-
-<body 
-    id="kt_body" 
-    style="<?= $hideBackground ?? env('WEB_BACKGROUND'); ?>" 
-    class="<?= $class ?? 'header-fixed header-tablet-and-mobile-fixed toolbar-enabled' ?>">
     <!--[if lt IE 7]>
-        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
