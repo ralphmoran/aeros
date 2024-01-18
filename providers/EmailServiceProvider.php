c91ebf4537af032e8ed0fa4ecddb5a93fd0f5023
ralph.moran <ralphmoran2003@gmail.com>
2024-01-18 11:30:15 -0800

    Clean up

--- providers/EmailServiceProvider.php
+++ providers/EmailServiceProvider.php
@@ -8,9 +8,7 @@ class EmailServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        # TODO: Add event listeners to cache, this way, they will be available and this process
-        // can be avoided for performance improvements.
-
+        //
         // Setup email connection
         // ...
     }
