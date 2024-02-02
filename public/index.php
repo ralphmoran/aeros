c13b4954e132162f0ff11ae2f940dffc9227baad
ralph.moran <ralphmoran2003@gmail.com>
2024-02-02 09:01:37 -0800

    Removed some TODOs

--- public/index.php
+++ public/index.php
@@ -10,14 +10,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-# TODO: Add languages support: translation
 # TODO: Use a different email sender system: PHPMailer is recommended
 # TODO: Review route loading. Is it necessary to load web and api routes? See: RouteServiceProvider
 # TODO: Update the ecosystem to catch errors, identify them and fix them on the fly. Example: "ERROR 'json' missing in Response class"
     // This error means that cache('myme.types') is null or not available
-# TODO: Implement Phinx for all drivers, also, use socket connections (ONLY if client and server are in the same server)
 # TODO: Use socket connections for MySQL to improve performance
-# TODO: Add broadcasting feature from backend to front end (Axios or Pusher)
-# TODO: Refactor file tree
+# TODO: Add broadcasting feature from backend to front end (Axios or Pusher): https://packagist.org/packages/cboden/ratchet
 
 app()->run();
