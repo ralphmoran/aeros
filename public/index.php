5964e81ad5d01c118be40cc959c7b717bf91abce
ralph.moran <ralphmoran2003@gmail.com>
2024-02-04 14:27:27 -0800

    Fixed a bug related to generic cron

--- public/index.php
+++ public/index.php
@@ -11,9 +11,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 # TODO: Use a different email sender system: PHPMailer is recommended
-# TODO: Review route loading. Is it necessary to load web and api routes? See: RouteServiceProvider
-# TODO: Update the ecosystem to catch errors, identify them and fix them on the fly. Example: "ERROR 'json' missing in Response class"
-    // This error means that cache('myme.types') is null or not available
 # TODO: Use socket connections for MySQL to improve performance
 # TODO: Add broadcasting feature from backend to front end (Axios or Pusher): https://packagist.org/packages/cboden/ratchet
 
