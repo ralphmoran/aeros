99829cf31c857f5f4be8711e9cbb01c59f7c2347
Ralph Moran <ralphmoran2003@gmail.com>
2024-02-26 11:07:49 -0800

    Delete app directory

--- app/Views/common/footer.php
+++
@@ -1,6 +0,0 @@
-
-        <script src=""></script>
-        <script src="/assets/js/scripts.js" async></script>
-        <script src="" defer></script>
-    </body>
-</html>
--- app/Views/pictures.php
+++
@@ -1,7 +0,0 @@
-<?php view('common.header')?>
-
-<img src="/assets/media/tahoe.jpeg" alt="Lake Tahoe, CA">
-<img src="/assets/media/tahoe.webp" alt="Lake Tahoe, CA">
-
-
-<?php view('common.footer')?>
