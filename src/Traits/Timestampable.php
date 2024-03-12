e55c39810214c5664b3b409812c9726f9d97f24c
Ralph Moran <ralphmoran2003@gmail.com>
2024-03-12 16:16:57 -0700

    Added Timestampable trait (#23)

---
+++ src/Traits/Timestampable.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Lib\Traits\Traits;
+
+/**
+ * Trait Timestampable
+ *
+ * This trait provides functionality to manage timestamps for creation, updating, and soft deletion.
+ *
+ * @package App\Lib\Traits\Traits
+ */
+trait Timestampable
+{
+    /**
+     * @var int|null The timestamp of when the record was created.
+     */
+    protected $created_at;
+
+    /**
+     * @var int|null The timestamp of when the record was last updated.
+     */
+    protected $updated_at;
+
+    /**
+     * @var int|null The timestamp of when the record was soft deleted.
+     */
+    protected $deleted_at;
+
+    /**
+     * Update the "updated_at" timestamp.
+     */
+    public function updatedAt(): void
+    {
+        $this->updated_at = time();
+    }
+
+    /**
+     * Set the "created_at" timestamp.
+     */
+    public function createdAt(): void
+    {
+        $this->created_at = time();
+    }
+
+    /**
+     * Check if the record has been soft deleted.
+     *
+     * @return bool True if the record has been soft deleted, false otherwise.
+     */
+    public function isDeleted(): bool
+    {
+        return $this->deleted_at !== null;
+    }
+
+    /**
+     * Soft delete the record by setting the "deleted_at" timestamp.
+     */
+    public function softDeleteAt(): void
+    {
+        $this->deleted_at = time();
+    }
+}
